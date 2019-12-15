6dc0eb2fc95e3955d364f2cf509e4cff629df3a8
Anfas PV <pvanfas.talrop@gmail.com>
2019-12-15 20:24:59 +0530

    update

--- api/v1/customers/views.py
+++ api/v1/customers/views.py
@@ -1,10 +1,12 @@
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from api.v1.customers.serializers import CustomerSerializer
 from customers.models import Customer
 from rest_framework.renderers import JSONRenderer
 from rest_framework.decorators import api_view, permission_classes, renderer_classes
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, IsAuthenticated
-from api.v1.customers.serializers import CustomerSerializer
 from rest_framework import status
+from api.v1.general.functions import generate_serializer_errors
+from main.functions import get_auto_id
 
 
 @api_view(['GET'])
@@ -46,3 +48,58 @@ def customer(request,pk):
             'message' : "Customer not found"
         }
     return Response(response_data, status=status.HTTP_200_OK)
+    
+    
+@api_view(['POST'])
+@permission_classes((IsAuthenticated,))
+@renderer_classes((JSONRenderer,))
+def create_customer(request):
+    serialized = CustomerSerializer(data=request.data)
+    if serialized.is_valid():
+        auto_id = get_auto_id(Customer)
+        serialized.save(auto_id=auto_id,creator=request.user,updater=request.user)
+        
+        response_data = {
+            "StatusCode" : 6000,
+            'data' : serialized.data
+        }
+        
+        return Response(response_data,status=status.HTTP_200_OK)
+    else:
+        response_data = {
+            "StatusCode" : 6000,
+            "message" : generate_serializer_errors(serialized._errors)
+        }
+        return Response(response_data,status=status.HTTP_200_OK)
+    
+    
+@api_view(['POST'])
+@permission_classes((IsAuthenticated,))
+@renderer_classes((JSONRenderer,))
+def edit_customer(request):
+    serialized = CustomerSerializer(data=request.data)
+    instance = None
+    if Customer.objects.filter(is_deleted=False,pk=pk).exists():
+        instance = Customer.objects.get(is_deleted=False,pk=pk)
+    if instance:
+        if serialized.is_valid():
+            serialized.update(instance,serialized.data)
+            response_data = {
+                "StatusCode" : 6000,
+                'data' : serialized.data
+            }
+            return Response(response_data,status=status.HTTP_200_OK)
+        else:
+            response_data = {
+                "StatusCode" : 6000,
+                "message" : generate_serializer_errors(serialized._errors)
+            }
+            return Response(response_data,status=status.HTTP_200_OK)
+    else:
+        response_data = {
+            "StatusCode" : 6000,
+            "message" : "Customer not found"
+        }
+        return Response(response_data,status=status.HTTP_200_OK)
+    
+            
